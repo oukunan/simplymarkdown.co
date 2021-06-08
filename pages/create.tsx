60f9b2ef66fcae410de5de102431c43dce78ca1f
Olly <kunanan.t@taskworld.com>
2021-06-08 22:18:28 +0700

    Introduce `react-markdown` package

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import ReactMarkdown from 'react-markdown'
+import styled from 'styled-components'
 
 import Layout from '../components/Layout'
 
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  gap: 24px;
+  padding: 20px 50px;
+`
+
+const Item = styled.div`
+  flex: 1;
+  border: 1px solid;
+  height: 100%;
+`
+
 export default function Create() {
-  return <Layout>Create page</Layout>
+  return (
+    <Layout>
+      <Container>
+        <Item>Hello</Item>
+        <Item>
+          <ReactMarkdown># Hello, *world*!</ReactMarkdown>
+        </Item>
+      </Container>
+    </Layout>
+  )
 }
