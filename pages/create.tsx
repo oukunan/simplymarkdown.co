f8b92291b7afab641178fdb5efdda6263c3232dc
Olly <kunanan.t@taskworld.com>
2021-06-08 17:28:37 +0700

    Add `create` page

---
+++ pages/create.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import Layout from '../components/Layout'
+
+export default function Create() {
+  return <Layout>Create page</Layout>
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
+import Link from 'next/link'
+
 import Layout from '../components/Layout'
 
 export default function Home() {
-  return <Layout>Home</Layout>
+  return (
+    <Layout>
+      <Link href="create">Go create new Readme.md</Link>
+    </Layout>
+  )
 }
