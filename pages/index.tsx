0de63d69ca809ca549755c1aac86be437a93972e
Olly <kunanan.t@taskworld.com>
2021-06-12 09:35:28 +0700

    Fix href Link

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import Layout from '../components/Layout'
 export default function Home() {
   return (
     <Layout>
-      <Link href="create">Go create new Readme.md</Link>
+      <Link href="/create">Go create new Readme.md</Link>
     </Layout>
   )
 }
