abb09df59929a2b17c5e84b103953b0d68147abd
Olly <kunanan.t@taskworld.com>
2021-06-17 14:34:11 +0700

    Add `next-seo` package

---
+++ next-seo.config.js
@@ -0,0 +1,34 @@
+export default {
+  defaultTitle: 'simplymarkdown.co - Simply markdown editor',
+  description:
+    'The quickest way to create a markdown file with live preview, custom template and instant download',
+  additionalMetaTags: [
+    {
+      property: 'keywords',
+      content: 'markdown, readme, editor',
+    },
+  ],
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://simplymarkdown.co',
+    title: 'simplymarkdown.co - Simply markdown editor',
+    description:
+      'The quickest way to create a markdown file with live preview, custom template and instant download',
+    images: [
+      {
+        url: 'https://simplymarkdown.co/paper.png',
+        width: 375,
+        height: 375,
+        alt: 'Open graph image',
+      },
+    ],
+    site_name: 'markdown.co',
+    imageWidth: '1200',
+    imageHeight: 1200,
+  },
+  twitter: {
+    handle: '@oukunanan',
+    cardType: 'summary',
+  },
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Modal from 'react-modal'
+import { DefaultSeo } from 'next-seo'
+
+import SEO from '../next-seo.config'
 import { GlobalStyle } from '../styles/global'
 
 Modal.setAppElement('#__next')
@@ -7,6 +10,7 @@ Modal.setAppElement('#__next')
 function MyApp({ Component, pageProps }: { Component: any; pageProps: any }) {
   return (
     <>
+      <DefaultSeo {...SEO} />
       <GlobalStyle />
       <Component {...pageProps} />
     </>
