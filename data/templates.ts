b3529f2a9b35f250c29e72d0fe3fe30d551f1662
Olly <kunanan.t@taskworld.com>
2021-06-17 16:52:06 +0700

    Update template

--- data/templates.ts
+++ data/templates.ts
@@ -1,19 +1,18 @@
 export const templates = {
-  apiReference: `
-## API Reference
+  apiReference: `## API Reference
             
-#### Get all items
+#### Get all users
             
 \`\`\`http
-    GET /api/items
+  GET /api/users
 \`\`\`
             
-| Parameter | Type     | Description                |
-| :-------- | :------- | :------------------------- |
-| \`api_key\` | \`string\` | **Required**. Your API key |
+| Attribute | Type        | Required  | Description             |
+| :-------- | :---------- | :-------- | :---------------------- |
+| \`user_id\`    | \`string\`  |   Yes     |   Put your description  |
+
 `,
-  pullRequest: `
-### Problems
+  pullRequest: `### Problems
       
 ### How would the reviewers verify the solutions
       
@@ -21,23 +20,22 @@ export const templates = {
       
 - [ ] **Manual testing** has been performed according to the acceptance criteria.
 - [ ] **Unit testing** and/or end-to-end testing has got covered.
+
 `,
-  contributing: `
-## Contributing
+  contributing: `## Contributing
   
 If you're interested in contributing, please read our contributing docs before submitting a pull request.
 
 `,
-  codeExample: `
-## Example
+  codeExample: `## Example
 \`\`\`
     function helloWorld() {
         return 'Hello world'
     }
 \`\`\`
+
 `,
-  collapsible: `
-<details>
+  collapsible: `<details>
   <summary>Click to expand</summary>
   
   ### Title
