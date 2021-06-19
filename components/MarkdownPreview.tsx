bab17130839c05f4e559e6e27def3a5c1c3651ea
Olly <kunanan.t@taskworld.com>
2021-06-19 20:55:34 +0700

    Hidden `::marker`

--- components/MarkdownPreview.tsx
+++ components/MarkdownPreview.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import MDEditor from '@uiw/react-md-editor'
 
+import styled from 'styled-components'
+
 type Props = {
   value: string
 }
 
+const Wrapper = styled(MDEditor.Markdown)`
+  height: 100%;
+  padding: 48px 24px;
+
+  // Make ::marker for this type hidden
+  ul .task-list-item::marker {
+    color: white;
+  }
+`
+
 export default function MarkdownPreview(props: Props) {
-  return (
-    <MDEditor.Markdown
-      source={props.value}
-      style={{
-        height: '100%',
-        padding: '48px 24px',
-      }}
-    />
-  )
+  return <Wrapper source={props.value} />
 }
