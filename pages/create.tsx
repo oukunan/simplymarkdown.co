3cd64de20fcdfe6db5f3a21b74b84c545022261d
futurouz <kunanan.tw@gmail.com>
2022-05-08 20:22:56 +0700

    Unused code

--- pages/create.tsx
+++ pages/create.tsx
@@ -111,18 +111,6 @@ export default function Create() {
   return (
     <>
       <Head>
-        <script
-          data-name="BMC-Widget"
-          data-cfasync="false"
-          src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-          data-id="oukunanan"
-          data-description="Support me on Buy me a coffee!"
-          data-message="Support me by buy me some coffee"
-          data-color="#FFDD00"
-          data-position="Right"
-          data-x_margin="18"
-          data-y_margin="18"
-        ></script>
       </Head>
       <Layout>
         <Wrapper>
